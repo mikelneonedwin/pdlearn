5851fcc0c25dff1fae585cfe822f3f83e453b517
Winner Edwin <winneredwin2667@gmail.com>
2026-03-26 15:07:06 +0100

    initial commit

---
+++ electron.vite.config.ts
@@ -0,0 +1,27 @@
+import tailwindcss from "@tailwindcss/vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "electron-vite"
+import { resolve } from "path"
+
+export default defineConfig({
+  main: {},
+  preload: {},
+  renderer: {
+    resolve: {
+      alias: {
+        "@renderer": resolve("src/renderer/src")
+      } 
+    },
+    plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true,
+        routesDirectory: "./src/routes",
+        generatedRouteTree: "./src/routeTree.gen.ts"
+      }),
+      react(),
+      tailwindcss()
+    ]
+  }
+})
---
+++ todo.md
@@ -0,0 +1,5 @@
+## Installations
+
+- lefthook
+- drizzle
+- shadcn
